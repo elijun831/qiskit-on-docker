ee82060f29f9b09234b49ceacf661914256d5f92
elijun831 <148694425+elijun831@users.noreply.github.com>
2024-07-06 23:44:07 +0900

    Update jupyter_lab_config.py

--- docker/jupyter_lab_config.py
+++ docker/jupyter_lab_config.py
@@ -1,5 +1,7 @@
+c = get_config() 
+
+c.NotebookApp.certfile = u'/home/appuser/.jupyter/localhost.pem'
+c.NotebookApp.keyfile = u'/home/appuser/.jupyter/localhost.key'
+c.NotebookApp.ip = '0.0.0.0'
 c.NotebookApp.open_browser = False
-c.NotebookApp.ip = '*'
-c.NotebookApp.port = 8888
-c.NotebookApp.certfile = u'/home/[Path]/ssl_cert/example.pem'
-c.NotebookApp.keyfile = u'/home/[Path]/ssl_cert/example.key'
+c.NotebookApp.allow_root = True
