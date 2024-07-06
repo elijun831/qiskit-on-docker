e7a7b1dc342633c8e32e6e991583f3d9ca192024
elijun831 <148694425+elijun831@users.noreply.github.com>
2024-07-06 23:28:11 +0900

    Create jupyter_lab_config.py

---
+++ docker/jupyter_lab_config.py
@@ -0,0 +1,5 @@
+c.NotebookApp.open_browser = False
+c.NotebookApp.ip = '*'
+c.NotebookApp.port = 8888
+c.NotebookApp.certfile = u'/home/[Path]/ssl_cert/example.pem'
+c.NotebookApp.keyfile = u'/home/[Path]/ssl_cert/example.key'
