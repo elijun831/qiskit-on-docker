a12216b4e9024e25586f784b30dd0eaa0b2b5a98
elijun831 <148694425+elijun831@users.noreply.github.com>
2024-07-07 17:50:04 +0900

    Update jupyter_lab_config.py

--- docker/jupyter_lab_config.py
+++ docker/jupyter_lab_config.py
@@ -1,6 +1,13 @@
-c.ServerApp.certfile = u'/home/appuser/ssl_cert/localhost.pem'
-c.ServerApp.keyfile = u'/home/appuser/ssl_cert/localhost.key'
+c = get_config()
+c.ServerApp.base_url = '/'
 c.ServerApp.ip = '0.0.0.0'
 c.ServerApp.open_browser = False
 c.ServerApp.port = 8888
 c.ServerApp.allow_root = True
+
+import secrets
+c.ServerApp.token = secrets.token_hex(32)
+c.ServerApp.root_dir = '/app/notebooks'
+c.ServerApp.certfile = '/home/appuser/ssl_cert/localhost.pem'
+c.ServerApp.keyfile = '/home/appuser/ssl_cert/localhost.key'
+c.ServerApp.use_https = True
